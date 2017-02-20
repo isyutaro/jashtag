310e7eb7e11b2e8591e01a99c0258cae34d944b4
web <web@intersys.mx>
2017-02-20 17:17:46 -0600

    especificar hashtag desde comando

--- jashtag/twitter/management/commands/twitterstream.py
+++ jashtag/twitter/management/commands/twitterstream.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import json
+from optparse import make_option
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -23,6 +24,11 @@ class TwitterStreamListener(StreamListener):
 
 class Command(BaseCommand):
     help = "Streams Twitter"
+
+    option_list = BaseCommand.option_list + (
+        make_option('--hashtag', dest='hashtag', help='Hashtag que se va a trackear'),
+    )
+
     def handle(self, **options):
         auth = OAuthHandler(
             settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET
@@ -34,9 +40,13 @@ class Command(BaseCommand):
         stream = Stream(
             auth=auth, listener=TwitterStreamListener()
         )
-        print '[-] Init stream: {}'.format(settings.TWITTER_HASH_TAG)
+
+        hashtag = options.get('hashtag')
+        hashtag = hashtag if hashtag else settings.TWITTER_HASH_TAG
+        hashtag = "#%s" % hashtag
+        print '[-] Init stream: {}'.format(hashtag)
         # stream.filter(follow=[settings.TWITTER_FOLLOW_USER])
         # When whe want to stream a hashtag use filter instade of userstream
         # stream.filter(track=[settings.TWITTER_HASH_TAG])
-        stream.filter(track=[settings.TWITTER_HASH_TAG])
+        stream.filter(track=[hashtag])
 
